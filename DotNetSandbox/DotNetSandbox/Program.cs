815921efe1135f182142f17ddb85007b68eecdcc
nickguerra <nickg@maliciousgames.com>
2022-11-20 07:10:37 -0600

    Added read only spans to verify address sections

--- DotNetSandbox/DotNetSandbox/Program.cs
+++ DotNetSandbox/DotNetSandbox/Program.cs
@@ -1,4 +1,16 @@
 ﻿
 
 string address = "1640 Riverside Drive";
-Console.WriteLine("Hello Keller");
+ReadOnlySpan<char> addressSpan = address;
+ReadOnlySpan<char> houseNumberSpan = addressSpan.Slice(0, 4);
+ReadOnlySpan<char> streeNameSpan = addressSpan.Slice(5, 9);
+ReadOnlySpan<char> streetTypeSpan = addressSpan.Slice(15, 5);
+
+string houseNumber = address.Substring(0, 4);
+string streetName = address.Substring(5, 9);
+string streetType = address.Substring(15, 5);
+
+Console.WriteLine(address);
+Console.WriteLine(houseNumber);
+Console.WriteLine(streetName);
+Console.WriteLine(streetType);
